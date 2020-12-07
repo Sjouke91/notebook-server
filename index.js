1d38f1b2551f70a7b15bd253c324c370a380f971
carlijnweijer <carlijnweijer@gmail.com>
2020-12-07 17:33:45 +0100

    Auth, login and signup router

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const loggerMiddleWare = require("morgan");
+app.use(loggerMiddleWare("dev"));
+
+const bodyParserMiddleWare = express.json();
+app.use(bodyParserMiddleWare);
+
+const corsMiddleWare = require("cors");
+app.use(corsMiddleWare());
+
+const authMiddleWare = require("./auth/middleware");
+
+const authRouter = require("./routers/auth");
+app.use("/", authRouter);
+
+// Listen for connections on specified port (default is port 4000)
+const { PORT } = require("./config/constants");
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
