e2ff8c7e125c8851534da3f2388531c81063477c
Sjouke91 <s.bosma1991@gmail.com>
2020-12-08 15:36:49 +0100

    Added endpoints to the notebooksrouter

--- routers/auth.js
+++ routers/auth.js
@@ -23,7 +23,7 @@ router.post("/login", async (req, res, next) => {
       where: { username },
       include: {
         model: Notebook,
-        attributes: ["name", "createdAt", "updatedAt"],
+        attributes: ["id", "name", "createdAt", "updatedAt"],
       },
     });
 
